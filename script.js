1c7fa8e105515274e2b7c3d9d4432e22697554b4
Abubakar Usman Fari <abubakrusmanfari@gmail.com>
2023-05-27 03:16:54 +0100

    Fun game

---
+++ script.js
@@ -0,0 +1,149 @@
+'use strict';
+// Selecting elements
+const displayedMove = document.querySelector('.hidden');
+const playerDisplay = document.querySelector('.player-display');
+const compDisplay = document.querySelector('.comp-display');
+const intro = document.querySelector('.intro');
+const message = document.querySelector('.message');
+let playerScoreE = document.querySelector('.player-score').textContent;
+let tieScoreE = document.querySelector('.tie').textContent;
+let compScoreE = document.querySelector('.computer-score').textContent;
+
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+const scissorsBtn = document.querySelector('.scissors');
+const resetBtn = document.querySelector('.reset-btn');
+
+let computerMove,
+  result = '';
+let tie = 0;
+let playerScore = 0;
+let compScore = 0;
+
+const generateAndDisplay = function () {
+  // Displaying the move
+  displayedMove.classList.remove('hidden');
+  intro.classList.add('hidden');
+  message.classList.remove('hidden');
+
+  // Generating a move for the computer
+  const randomNumber = Math.trunc(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
+    computerMove = 'rock';
+    compDisplay.src = './img/rock-emoji.png';
+  } else if (randomNumber === 2) {
+    computerMove = 'paper';
+    compDisplay.src = './img/paper-emoji.png';
+  } else if (randomNumber === 3) {
+    computerMove = 'scissors';
+    compDisplay.src = './img/scissors-emoji.png';
+  }
+};
+
+rockBtn.addEventListener('click', function () {
+  generateAndDisplay();
+  playerDisplay.src = './img/rock-emoji.png';
+  // Comparing computer and player moves
+
+  if (computerMove === 'rock') {
+    result = 'Tie!';
+    document.querySelector(
+      '.message'
+    ).textContent = `You picked rock, computer picked ${computerMove}. ${result}`;
+    tie += 1;
+    document.querySelector('.tie').textContent = tie;
+  } else if (computerMove === 'paper') {
+    result = 'You lost!';
+    document.querySelector(
+      '.message'
+    ).textContent = `You picked rock, computer picked ${computerMove}. ${result}`;
+    compScore += 1;
+    document.querySelector('.computer-score').textContent = compScore;
+  } else if (computerMove === 'scissors') {
+    result = 'You won!';
+    document.querySelector(
+      '.message'
+    ).textContent = `You picked rock, computer picked ${computerMove}. ${result}`;
+    playerScore += 1;
+    document.querySelector('.player-score').textContent = playerScore;
+  }
+
+  console.log(computerMove);
+  console.log(playerScore);
+});
+
+paperBtn.addEventListener('click', function () {
+  generateAndDisplay();
+  playerDisplay.src = './img/paper-emoji.png';
+
+  if (computerMove === 'rock') {
+    result = 'You won!';
+    document.querySelector(
+      '.message'
+    ).textContent = `You picked paper, computer picked ${computerMove}. ${result}`;
+    playerScore += 1;
+    document.querySelector('.player-score').textContent = playerScore;
+  } else if (computerMove === 'paper') {
+    result = 'Tie!';
+    document.querySelector(
+      '.message'
+    ).textContent = `You picked paper, computer picked ${computerMove}. ${result}`;
+    tie += 1;
+    document.querySelector('.tie').textContent = tie;
+  } else if (computerMove === 'scissors') {
+    result = 'You lost!';
+    document.querySelector(
+      '.message'
+    ).textContent = `You picked paper, computer picked ${computerMove}. ${result}`;
+    compScore += 1;
+    document.querySelector('.computer-score').textContent = compScore;
+  }
+
+  console.log(computerMove);
+  console.log(playerScore);
+});
+
+scissorsBtn.addEventListener('click', function () {
+  generateAndDisplay();
+  playerDisplay.src = './img/scissors-emoji.png';
+
+  if (computerMove === 'rock') {
+    result = 'You lost!';
+    document.querySelector(
+      '.message'
+    ).textContent = `You picked scissors, computer picked ${computerMove}. ${result}`;
+    compScore += 1;
+    document.querySelector('.computer-score').textContent = compScore;
+  } else if (computerMove === 'paper') {
+    result = 'You won!';
+    document.querySelector(
+      '.message'
+    ).textContent = `You picked scissors, computer picked ${computerMove}. ${result}`;
+    playerScore += 1;
+    document.querySelector('.player-score').textContent = playerScore;
+  } else if (computerMove === 'scissors') {
+    result = 'Tie!';
+    document.querySelector(
+      '.message'
+    ).textContent = `You picked scissors, computer picked ${computerMove}. ${result}`;
+    tie += 1;
+    document.querySelector('.player-score').textContent = tie;
+  }
+
+  console.log(computerMove);
+  console.log(playerScore);
+});
+
+resetBtn.addEventListener('click', function () {
+  displayedMove.classList.add('hidden');
+  intro.classList.remove('hidden');
+  message.classList.add('hidden');
+
+  tie = 0;
+  playerScore = 0;
+  compScore = 0;
+
+  document.querySelector('.tie').textContent = tie;
+  document.querySelector('.player-score').textContent = playerScore;
+  document.querySelector('.computer-score').textContent = compScore;
+});
